648db23303c0693857250ada4583768a8111e8da
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-07-12 20:45:07 -0400

    added angular animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,7 +16,8 @@ import { ProjectComponent } from './components/project/project.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/project/project.component.ts
+++ src/app/components/project/project.component.ts
@@ -1,10 +1,36 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, Input, OnInit } from '@angular/core';
 import Project from 'src/Project';
 
 @Component({
   selector: 'app-project',
   templateUrl: './project.component.html',
-  styleUrls: ['./project.component.scss']
+  styleUrls: ['./project.component.scss'],
+  animations: [
+    trigger("reveal", [
+      state("shown", style({
+        transform:'scaleY(1)',
+        display: 'flex'
+      })),
+      state("hidden", style({
+        transform: 'scaleY(0)',
+      })),
+      transition("hidden => shown", animate("0ms ease-in-out")),
+      transition("shown => hidden", animate("300ms ease-in-out")),
+    ]),
+    trigger("hover", [
+      state('notHover', style({
+        margin: '1rem 0',
+        padding: '0 1rem',
+      })),
+      state('hover', style({
+        margin: '1rem 0 1rem',
+        padding: '1rem 2rem',
+      })),
+      transition('hover => notHover', animate('300ms ease-in-out')),
+      transition('notHover => hover', animate('300ms ease-in-out')),
+    ]),
+  ]
 })
 export class ProjectComponent implements OnInit {
   @Input() project!: Project;
@@ -12,16 +38,26 @@ export class ProjectComponent implements OnInit {
   repository!: string;
   photoPath!: string;
   visible: boolean = false;
+  hover: boolean = false;
   constructor() { }
 
   ngOnInit(): void {
     this.liveServer = `https://collerhatguy.github.io/${this.project.repoName}/`;
     this.repository = `https://github.com/collerhatguy/${this.project.repoName}/`;
     this.photoPath = `assets/projectPhotos/${this.project.repoName}.png`
-    console.log(this.project)
   }
 
   reveal() {
     this.visible = !this.visible;
   }
+  revealState() {
+    return this.visible ? 'shown' : 'hidden';
+  }
+  handleHoverChange() {
+    this.hover = !this.hover;
+  }
+  handleHover() {
+    return this.hover ? 'hover' : 'notHover';
+  }
+
 }
