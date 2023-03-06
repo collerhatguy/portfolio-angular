cdd661d7f88fd454709d0bc1cbb51cf9293dd2ef
jacob <jshomali@nexient.com>
2023-03-05 20:04:44 -0600

    added new project

--- src/data/project-data.ts
+++ src/data/project-data.ts
@@ -1,105 +1,128 @@
-import Project from "./Project";
+import Project from './Project';
 const projects: Project[] = [
-    {
-        repoName: "org-net2",
-        siteName: "Org Chart Api",       
-        repository: `https://github.com/collerhatguy/org-net2`,
-        technologies: [".Net 6", "C#", "SQL", "Enitity Framework Core"]
-    },
-    {
-        repoName: "dan-schneider-nicknames",
-        siteName: "Dan Schneider Nicknames Site",       
-        repository: `https://github.com/dan-schneider-nicknames`,
-        liveServer: "https://dan-schneider-nicknames.github.io/frontend/",
-        technologies: ["React", "JavaScript", "React-Router", "Node", "GraphQL", "Express", "JWT"]
-    },
-    {
-        repoName: "human-rights-first-docdb-fe",
-        siteName: "Human Right First Document Database",       
-        repository: `https://github.com/BloomTech-Labs/human-rights-first-docdb-fe`,
-        technologies: ["React", "JavaScript", "React-Router", "REDUX", "CSS"]
-    },
-    {
-        repoName: "potlucks",
-        siteName: "Potlucks Website",       
-        repository: `https://github.com/Build-Week-Potluck-Planner-4`,
-        technologies: ["React", "REDUX", "Node", "Express", "SQL", "Postgres", "API"]
-    },
-    {
-        siteName: "Rock, Paper, Scissors Game",
-        repoName: "rock-paper-scissors",
-        repository: `https://github.com/collerhatguy/rock-paper-scissors/`,
-        liveServer: `https://collerhatguy.github.io/rock-paper-scissors/`,
-        technologies: ["React", "JavaScript", "SCSS", "REDUX"]
-    },
-    {
-        repoName:"countries",
-        siteName: "Countries Website",
-        repository: `https://github.com/collerhatguy/countries/`,
-        technologies: ["React", "REDUX", "React-Router", "SCSS", "API"]
-    },
-    {
-        repoName:"todo-angular",
-        siteName: "Jacob's Todo List",
-        repository: `https://github.com/collerhatguy/todo-angular/`,
-        liveServer: `https://collerhatguy.github.io/todo-angular/`,
-        technologies: ["Angular", "Typscript", "SCSS", "REDUX"]
-    },
-    {
-        repoName:"crowd-funding-frontend-mentor-3",
-        siteName: "Crowd Funding Site",
-        repository: `https://github.com/collerhatguy/crowd-funding-frontend-mentor-3/`,
-        liveServer: `https://collerhatguy.github.io/crowd-funding-frontend-mentor-3/`,
-        technologies: ["React", "JavaScript", "SCSS", "REDUX"]
-    },
-    {
-        repoName: "coal-miner-fixed",
-        siteName: "Gold Miner Game",
-        repository: `https://github.com/collerhatguy/coal-miner-fixed/`,
-        liveServer: `https://collerhatguy.github.io/coal-miner-fixed/`,
-        technologies: ["React", "JavaScript", "SCSS"]
-    },
-    {
-        repoName: "hearthstone-react-2",
-        siteName: "Hearthstone Cards",
-        repository: `https://github.com/collerhatguy/hearthstone-react-2/`,
-        liveServer: `https://collerhatguy.github.io/hearthstone-react-2/`,
-        technologies: ["React", "React-Router", "SCSS", "API"]
-    },
-    {
-        repoName: "easy-bank-landing",
-        siteName: "Easy Bank Landing",
-        repository: `https://github.com/collerhatguy/easy-bank-landing/`,
-        liveServer: `https://collerhatguy.github.io/easy-bank-landing/`,
-        technologies: ["Angular", "SCSS"]
-    },
-    {
-        repoName: "face-maker-2",
-        siteName: "Face Maker App",
-        repository: `https://github.com/collerhatguy/face-maker-2/`,
-        liveServer: `https://collerhatguy.github.io/face-maker-2/`,
-        technologies: ["React", "JavaScript", "SCSS"]
-    },
-    {
-        repoName: "countdown-app",
-        siteName: "Countdown App",       
-        repository: `https://github.com/collerhatguy/countdown-app/`,
-        liveServer: `https://collerhatguy.github.io/countdown-app/`,
-        technologies: ["React", "JavaScript", "SCSS"]
-    },
-    {
-        repoName: "calculator",
-        siteName: "Calculator App",     
-        repository: `https://github.com/collerhatguy/calculator/`,
-        liveServer: `https://collerhatguy.github.io/calculator/`,
-        technologies: ["React", "JavaScript", "SCSS"]
-    },
-    {
-        repoName: "zelda",
-        siteName: "Zelda Info Site",           
-        repository: `https://github.com/collerhatguy/zelda/`,
-        liveServer: `https://collerhatguy.github.io/zelda/`,
-        technologies: ["React", "JavaScript", "SCSS", "API"]
-    },
-]
+  {
+    repoName: 'tetris-game',
+    siteName: 'Tetris Remake',
+    repository: `https://github.com/collerhatguy/tetris-game`,
+    liveServer: 'https://collerhatguy.github.io/tetris-game/',
+    technologies: ['Angular', 'Typscript', 'SCSS', 'RXJS'],
+  },
+  {
+    repoName: 'org-net2',
+    siteName: 'Org Chart Api',
+    repository: `https://github.com/collerhatguy/org-net2`,
+    technologies: ['.Net 6', 'C#', 'SQL', 'Enitity Framework Core'],
+  },
+  {
+    repoName: 'dan-schneider-nicknames',
+    siteName: 'Dan Schneider Nicknames Site',
+    repository: `https://github.com/dan-schneider-nicknames`,
+    liveServer: 'https://dan-schneider-nicknames.github.io/frontend/',
+    technologies: [
+      'React',
+      'JavaScript',
+      'React-Router',
+      'Node',
+      'GraphQL',
+      'Express',
+      'JWT',
+    ],
+  },
+  {
+    repoName: 'human-rights-first-docdb-fe',
+    siteName: 'Human Right First Document Database',
+    repository: `https://github.com/BloomTech-Labs/human-rights-first-docdb-fe`,
+    technologies: ['React', 'JavaScript', 'React-Router', 'REDUX', 'CSS'],
+  },
+  {
+    repoName: 'potlucks',
+    siteName: 'Potlucks Website',
+    repository: `https://github.com/Build-Week-Potluck-Planner-4`,
+    technologies: [
+      'React',
+      'REDUX',
+      'Node',
+      'Express',
+      'SQL',
+      'Postgres',
+      'API',
+    ],
+  },
+  {
+    siteName: 'Rock, Paper, Scissors Game',
+    repoName: 'rock-paper-scissors',
+    repository: `https://github.com/collerhatguy/rock-paper-scissors/`,
+    liveServer: `https://collerhatguy.github.io/rock-paper-scissors/`,
+    technologies: ['React', 'JavaScript', 'SCSS', 'REDUX'],
+  },
+  {
+    repoName: 'countries',
+    siteName: 'Countries Website',
+    repository: `https://github.com/collerhatguy/countries/`,
+    technologies: ['React', 'REDUX', 'React-Router', 'SCSS', 'API'],
+  },
+  {
+    repoName: 'todo-angular',
+    siteName: "Jacob's Todo List",
+    repository: `https://github.com/collerhatguy/todo-angular/`,
+    liveServer: `https://collerhatguy.github.io/todo-angular/`,
+    technologies: ['Angular', 'Typscript', 'SCSS', 'REDUX'],
+  },
+  {
+    repoName: 'crowd-funding-frontend-mentor-3',
+    siteName: 'Crowd Funding Site',
+    repository: `https://github.com/collerhatguy/crowd-funding-frontend-mentor-3/`,
+    liveServer: `https://collerhatguy.github.io/crowd-funding-frontend-mentor-3/`,
+    technologies: ['React', 'JavaScript', 'SCSS', 'REDUX'],
+  },
+  {
+    repoName: 'coal-miner-fixed',
+    siteName: 'Gold Miner Game',
+    repository: `https://github.com/collerhatguy/coal-miner-fixed/`,
+    liveServer: `https://collerhatguy.github.io/coal-miner-fixed/`,
+    technologies: ['React', 'JavaScript', 'SCSS'],
+  },
+  {
+    repoName: 'hearthstone-react-2',
+    siteName: 'Hearthstone Cards',
+    repository: `https://github.com/collerhatguy/hearthstone-react-2/`,
+    liveServer: `https://collerhatguy.github.io/hearthstone-react-2/`,
+    technologies: ['React', 'React-Router', 'SCSS', 'API'],
+  },
+  {
+    repoName: 'easy-bank-landing',
+    siteName: 'Easy Bank Landing',
+    repository: `https://github.com/collerhatguy/easy-bank-landing/`,
+    liveServer: `https://collerhatguy.github.io/easy-bank-landing/`,
+    technologies: ['Angular', 'SCSS'],
+  },
+  {
+    repoName: 'face-maker-2',
+    siteName: 'Face Maker App',
+    repository: `https://github.com/collerhatguy/face-maker-2/`,
+    liveServer: `https://collerhatguy.github.io/face-maker-2/`,
+    technologies: ['React', 'JavaScript', 'SCSS'],
+  },
+  {
+    repoName: 'countdown-app',
+    siteName: 'Countdown App',
+    repository: `https://github.com/collerhatguy/countdown-app/`,
+    liveServer: `https://collerhatguy.github.io/countdown-app/`,
+    technologies: ['React', 'JavaScript', 'SCSS'],
+  },
+  {
+    repoName: 'calculator',
+    siteName: 'Calculator App',
+    repository: `https://github.com/collerhatguy/calculator/`,
+    liveServer: `https://collerhatguy.github.io/calculator/`,
+    technologies: ['React', 'JavaScript', 'SCSS'],
+  },
+  {
+    repoName: 'zelda',
+    siteName: 'Zelda Info Site',
+    repository: `https://github.com/collerhatguy/zelda/`,
+    liveServer: `https://collerhatguy.github.io/zelda/`,
+    technologies: ['React', 'JavaScript', 'SCSS', 'API'],
+  },
+];
 export default projects;
